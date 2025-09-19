cfa5835978af62be34452ba8665189c95335a8c4
tomadriaens <tomadriaens@hotmail.com>
2025-09-19 14:22:01 +0200

    [LES] velden en Properties

---
+++ 02_TomA_VolgendeVakantie_Klasses/01_TomA_VolgendeVakantie/Vakantie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_TomA_VolgendeVakantie
+{
+    internal class Vakantie
+    {
+        // Velden (Fields)
+        String _vakantienaam = null;
+        DateTime _startDatum= DateTime.MinValue;
+        DateTime _eindDatum = DateTime.MinValue;
+
+        // Eigenschappen (Properties)
+        public String PropVakantienaam
+        {
+            get { return _vakantienaam; }
+            set { _vakantienaam = value; }
+        }
+        public DateTime PropStartDatum
+        {
+            get { return _startDatum; }
+            set { _startDatum = value; }
+        }
+        
+        public DateTime PropEindDatum
+        {
+            get { return _eindDatum; }
+            set { _eindDatum = value; }
+        }
+
+        // Methoden (Methods)
+
+        // Constructors
+
+    }
+}
