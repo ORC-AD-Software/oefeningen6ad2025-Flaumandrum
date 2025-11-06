255fbd2e780c0fb853178e3059ec8669b2993532
tomadriaens <tomadriaens@hotmail.com>
2025-11-06 14:42:55 +0100

    [LES] toevoegen speler

--- 03_TomA_Minigolf/03_TomA_Minigolf/PersistentNaarDB.cs
+++ 03_TomA_Minigolf/03_TomA_Minigolf/PersistentNaarDB.cs
@@ -10,7 +10,7 @@ namespace _03_TomA_Minigolf
     internal class PersistentNaarDB
     {
         // Velden
-        static String _mijnPad = "";
+        static String _mijnPad = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\School\Romero\23-24-25\2. Klassen\6_AD_ApplicatieEnDatabeheer\Oefeningen\Databank\Minigolf.accdb";
         OleDbConnection _mijnConnectie = new OleDbConnection(_mijnPad);
 
         // Porperties (niet deel van deze cursus)
@@ -22,7 +22,7 @@ namespace _03_TomA_Minigolf
         /// <returns></returns>
         public List<Speler> GetSpelers()
         { 
-        
+            return new List<Speler>();
         }
 
         /// <summary>
@@ -31,6 +31,20 @@ namespace _03_TomA_Minigolf
         /// <param name="ontvSpeler">object van type speler</param>
         public void DbToevoegenSpeler(Speler ontvSpeler)
         {
+            // neem de nodige gegevens uit het object ontvSpeler
+            string voornaam = ontvSpeler.PropVoornaam;
+            string naam = ontvSpeler.PropNaam;
+            
+            // maak een sql query om de speler toe te voegen aan de database
+            string sqlQuery = $"INSERT INTO Speler (Voornaam, Familienaam, TotaalSlagen) VALUES ('{voornaam}', '{naam}', 0)";
+
+            // maak een commando om de query uit te voeren
+            OleDbCommand mijnCommand = new OleDbCommand(sqlQuery, _mijnConnectie);
+
+            // open de connectie, voer het commando uit en sluit de connectie
+            _mijnConnectie.Open();
+            mijnCommand.ExecuteNonQuery();
+            _mijnConnectie.Close();
 
         }
 
