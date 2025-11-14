ff8e3e931f402f135477081c6bdffc95acc0ed74
tomadriaens <tomadriaens@hotmail.com>
2025-11-14 12:56:38 +0100

    [LES] Aanmaken klasse start

---
+++ 04_TomA_BedrAfd_Klasses/04_TomA_BedrAfd_Klasses/Afdeling.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_TomA_BedrAfd_Klasses
+{
+    public class Afdeling
+    {
+        // Velden
+        int _id = 0;
+        string _afdelingsnaam = "";
+        string _afdelingshoofd = "";
+        List<Medewerker> _medewerkers = new List<Medewerker>();
+
+        // Properties
+
+        // Methoden
+        /// <summary>
+        /// Geeft de naam van de afdeling en het hoofd van de afdeling terug.
+        /// </summary>
+        /// <returns></returns>
+        public string GetGegevensAfdeling() 
+        {
+            return $"{_afdelingsnaam} - Hoofd: {_afdelingshoofd}";
+        }
+        /// <summary>
+        /// Geef een lijst (als tekst) van medewerkers in de afdeling terug.
+        /// </summary>
+        /// <returns></returns>
+        public string GetMedewerkers()
+        {
+            string antwoord = "";
+            foreach(Medewerker m in _medewerkers)
+            {
+               antwoord += m.GetVolledigeNaam() + "\n";
+            }
+
+            return antwoord;
+        }
+
+        // Constructors
+    }
+}
---
+++ 04_TomA_BedrAfd_Klasses/04_TomA_BedrAfd_Klasses/Medewerker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_TomA_BedrAfd_Klasses
+{
+    public class Medewerker
+    {
+        // Velden 
+        int _id = 0;
+        string _voornaam = "";
+        string _achternaam = "";
+        string statuut= "";
+        Afdeling _afdeling = new Afdeling();
+
+        // Properties
+
+
+        // Methoden
+        public string GetAfdelingEnHoofd() 
+        { 
+            return _afdeling.GetGegevensAfdeling();
+        }
+        public string GetVolledigeNaam()
+        {
+            return $"{_voornaam} {_achternaam}";
+        }
+        public string GetNaamStatuut()
+        {
+            return $"{_voornaam} {_achternaam} - {statuut}";
+        }
+        public string GetAlleGegevensMedewerker()
+        {
+            String antwoord = "";
+            
+            antwoord += $"{GetNaamStatuut()}\n   Afdeling {GetAfdelingEnHoofd()}";
+
+            return antwoord;
+        }
+
+        // Constructors
+
+    }
+}
